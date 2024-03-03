943384ef1bfa159d009e9f19a64accd745ab711d
Michal Turczyn <turek1992@o2.pl>
2024-03-03 15:22:25 +0100

    Improved look og navbar.

--- components/nav.tsx
+++ components/nav.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export default function Nav({ translation }: Props) {
   return (
-    <nav className="absolute flex h-16 w-full items-center border-b border-b-neutral-200 bg-white px-8 lg:px-96 [&>a]:mr-5">
+    <nav className="absolute flex h-16 w-full items-center border-b border-b-neutral-200 bg-white px-8 lg:px-96 [&>a:hover]:bg-gray-200 [&>a]:p-3">
       <Link href="/">{translation('nav.home-page')}</Link>
       <Link href="/contact-info">{translation('nav.contact-info')}</Link>
       <Link href="/technology-stack">
