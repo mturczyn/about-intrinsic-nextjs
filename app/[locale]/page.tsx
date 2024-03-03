2dc09b17f97a2076d9ed2fdd68cc94773a9f03c9
Michal Turczyn <turek1992@o2.pl>
2024-03-03 15:07:36 +0100

    Fixed issue with author image on home page. Created technology stack page. Added more content.

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,6 +1,7 @@
 import { Locale } from '@/i18n';
 import getTranslation from '@/lib/i18n/getTranslation';
 import Image from 'next/image';
+import authorImage from '../../public/author-image.jpg';
 
 type Props = {
   params: { locale: Locale };
@@ -10,20 +11,22 @@ export default async function Page({ params }: Props) {
   const translation = await getTranslation(params.locale);
 
   return (
-    <section className="flex h-96 flex-col items-center justify-center gap-4">
-      <h1 className="text-4xl font-medium tracking-tight">
-        {translation('views.home.title')}
-      </h1>
+    // <section className="flex h-96 flex-col items-center justify-center gap-4">
+    <>
       <Image
-        src="/author-image.jpg"
-        width={200}
-        height={200}
+        src={authorImage}
+        className="float-right m-5"
+        height={250}
         alt="Michał Turczyn"
         loading="eager"
       ></Image>
+      <h1 className="text-4xl font-medium tracking-tight">
+        {translation('views.home.title')}
+      </h1>
       <p className="align-middle text-xl font-light tracking-tight text-neutral-600 decoration-wavy">
         {translation('views.home.body')}
       </p>
-    </section>
+    </>
+    // </section>
   );
 }
--- components/nav.tsx
+++ components/nav.tsx
@@ -8,9 +8,12 @@ interface Props {
 
 export default function Nav({ translation }: Props) {
   return (
-    <nav className="absolute flex h-16 w-full items-center border-b border-b-neutral-200 bg-white px-8 lg:px-96">
+    <nav className="absolute flex h-16 w-full items-center border-b border-b-neutral-200 bg-white px-8 lg:px-96 [&>a]:mr-5">
       <Link href="/">{translation('nav.home-page')}</Link>
       <Link href="/contact-info">{translation('nav.contact-info')}</Link>
+      <Link href="/technology-stack">
+        {translation('nav.technology-stack')}
+      </Link>
       <LocaleSelector message={translation('nav.locale-selector.message')} />
     </nav>
   );
