61049c466ca11b83b986200ee30acb9031ce2b88
Michal Turczyn <turek1992@o2.pl>
2024-03-08 17:09:30 +0100

    Added sticky position to navbar.

--- components/nav.tsx
+++ components/nav.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 export default async function Nav({ params }: { params: { locale: string } }) {
     const { t } = await useTranslation(params.locale)
     return (
-        <nav className="absolute flex h-16 w-full items-center border-b border-b-neutral-200 bg-white px-8 lg:px-96 [&>a]:rounded-lg [&>a:hover]:bg-neutral-200 [&>a]:p-3 [&>a]:transition-colors [&>a]:duration-1000">
+        <nav className="sticky top-0 flex h-16 w-full items-center border-b border-b-neutral-200 bg-white px-8 lg:px-96 [&>a]:rounded-lg [&>a:hover]:bg-neutral-200 [&>a]:p-3 [&>a]:transition-colors [&>a]:duration-1000">
             <Link href="/">{t('nav.home')}</Link>
             <Link href="/contact-info">{t('nav.contact-info')}</Link>
             <Link href="/technology-stack">{t('nav.technology-stack')}</Link>
